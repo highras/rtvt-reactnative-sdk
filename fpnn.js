ac30ce402f601e7bb98b976bb9380010b233673c
jianjun.zhao <junstor@gmail.com>
2024-12-12 11:05:30 +0800

    init

---
+++ fpnn.js
@@ -0,0 +1,32 @@
+'use strict'
+
+//const FPClient = require('./fpnn/FPClient');
+//const FPConfig = require('./fpnn/FPConfig');
+//const FPEvent = require('./fpnn/FPEvent');
+//const FPSocket = require('./fpnn/FPSocket');
+//const FPPackage = require('./fpnn/FPPackage');
+//const FPCallback = require('./fpnn/FPCallback');
+//const FPProcessor = require('./fpnn/FPProcessor');
+//const FPError = require('./fpnn/FPError');
+
+import FPClient from './fpnn/FPClient';
+import FPConfig from './fpnn/FPConfig';
+import FPEvent from './fpnn/FPEvent';
+import FPSocket from './fpnn/FPSocket';
+import FPPackage from './fpnn/FPPackage';
+import FPCallback from './fpnn/FPCallback';
+import FPProcessor from './fpnn/FPProcessor';
+import FPError from './fpnn/FPError';
+
+export {
+	FPClient,
+	FPConfig,
+	FPEvent,
+	FPSocket,
+	FPPackage,
+	FPCallback,
+	FPProcessor,
+	FPError,
+};
+
+export default FPClient;
